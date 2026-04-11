5d87a0edb2a68922e5c525bd722bff5d81e2f0de
Dat Pham <thanhdattaycoc@gmail.com>
2026-04-11 18:15:49 +0700

    init project

--- BTLQUANLYSINHVIEN/Form1.cs
+++ BTLQUANLYSINHVIEN/Form1.cs
@@ -16,5 +16,10 @@ namespace BTLQUANLYSINHVIEN
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
