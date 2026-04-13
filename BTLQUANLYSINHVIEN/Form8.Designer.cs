00edfabb84284b536bdcde5d533fa10d164221bc
Dat Pham <thanhdattaycoc@gmail.com>
2026-04-13 12:02:45 +0700

    Thêm Form Đăng Ký Lớp

--- BTLQUANLYSINHVIEN/Form8.Designer.cs
+++ BTLQUANLYSINHVIEN/Form8.Designer.cs
@@ -28,19 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblMaSinhVien = new System.Windows.Forms.Label();
+            this.lblTenSinhVien = new System.Windows.Forms.Label();
+            this.cboMaLop = new System.Windows.Forms.ComboBox();
+            this.lblMalop = new System.Windows.Forms.Label();
+            this.lblNgayHomNay = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblCon = new System.Windows.Forms.Label();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnQuayLai = new System.Windows.Forms.Button();
+            this.lblLopDangKY = new System.Windows.Forms.Label();
+            this.dataGridViewLopDaDangKy = new System.Windows.Forms.DataGridView();
+            this.cboHuyDangKy = new System.Windows.Forms.ComboBox();
+            this.btnHuyDangKy = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLopDaDangKy)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblMaSinhVien
+            // 
+            this.lblMaSinhVien.AutoSize = true;
+            this.lblMaSinhVien.Location = new System.Drawing.Point(48, 24);
+            this.lblMaSinhVien.Name = "lblMaSinhVien";
+            this.lblMaSinhVien.Size = new System.Drawing.Size(84, 16);
+            this.lblMaSinhVien.TabIndex = 0;
+            this.lblMaSinhVien.Text = "Mã sinh viên:";
+            // 
+            // lblTenSinhVien
+            // 
+            this.lblTenSinhVien.AutoSize = true;
+            this.lblTenSinhVien.Location = new System.Drawing.Point(48, 57);
+            this.lblTenSinhVien.Name = "lblTenSinhVien";
+            this.lblTenSinhVien.Size = new System.Drawing.Size(86, 16);
+            this.lblTenSinhVien.TabIndex = 1;
+            this.lblTenSinhVien.Text = "Tên sinh viên";
+            // 
+            // cboMaLop
+            // 
+            this.cboMaLop.FormattingEnabled = true;
+            this.cboMaLop.Location = new System.Drawing.Point(175, 125);
+            this.cboMaLop.Name = "cboMaLop";
+            this.cboMaLop.Size = new System.Drawing.Size(205, 24);
+            this.cboMaLop.TabIndex = 2;
+            this.cboMaLop.SelectedIndexChanged += new System.EventHandler(this.cboMaLop_SelectedIndexChanged);
+            // 
+            // lblMalop
+            // 
+            this.lblMalop.AutoSize = true;
+            this.lblMalop.Location = new System.Drawing.Point(50, 125);
+            this.lblMalop.Name = "lblMalop";
+            this.lblMalop.Size = new System.Drawing.Size(51, 16);
+            this.lblMalop.TabIndex = 3;
+            this.lblMalop.Text = "Mã lớp:";
+            // 
+            // lblNgayHomNay
+            // 
+            this.lblNgayHomNay.AutoSize = true;
+            this.lblNgayHomNay.Location = new System.Drawing.Point(48, 88);
+            this.lblNgayHomNay.Name = "lblNgayHomNay";
+            this.lblNgayHomNay.Size = new System.Drawing.Size(97, 16);
+            this.lblNgayHomNay.TabIndex = 4;
+            this.lblNgayHomNay.Text = "Ngày hôm nay:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(718, 196);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // lblCon
+            // 
+            this.lblCon.AutoSize = true;
+            this.lblCon.Location = new System.Drawing.Point(183, 168);
+            this.lblCon.Name = "lblCon";
+            this.lblCon.Size = new System.Drawing.Size(37, 16);
+            this.lblCon.TabIndex = 6;
+            this.lblCon.Text = "Còn: ";
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(291, 163);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(89, 27);
+            this.btnDangKy.TabIndex = 7;
+            this.btnDangKy.Text = "Đăng ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(670, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnQuayLai
+            // 
+            this.btnQuayLai.Location = new System.Drawing.Point(352, 416);
+            this.btnQuayLai.Name = "btnQuayLai";
+            this.btnQuayLai.Size = new System.Drawing.Size(75, 23);
+            this.btnQuayLai.TabIndex = 9;
+            this.btnQuayLai.Text = "Quay lại";
+            this.btnQuayLai.UseVisualStyleBackColor = true;
+            // 
+            // lblLopDangKY
+            // 
+            this.lblLopDangKY.AutoSize = true;
+            this.lblLopDangKY.Location = new System.Drawing.Point(470, 35);
+            this.lblLopDangKY.Name = "lblLopDangKY";
+            this.lblLopDangKY.Size = new System.Drawing.Size(100, 16);
+            this.lblLopDangKY.TabIndex = 10;
+            this.lblLopDangKY.Text = "Lớp đã đăng ký";
+            // 
+            // dataGridViewLopDaDangKy
+            // 
+            this.dataGridViewLopDaDangKy.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLopDaDangKy.Location = new System.Drawing.Point(470, 57);
+            this.dataGridViewLopDaDangKy.Name = "dataGridViewLopDaDangKy";
+            this.dataGridViewLopDaDangKy.RowHeadersWidth = 51;
+            this.dataGridViewLopDaDangKy.RowTemplate.Height = 24;
+            this.dataGridViewLopDaDangKy.Size = new System.Drawing.Size(299, 94);
+            this.dataGridViewLopDaDangKy.TabIndex = 11;
+            // 
+            // cboHuyDangKy
+            // 
+            this.cboHuyDangKy.FormattingEnabled = true;
+            this.cboHuyDangKy.Location = new System.Drawing.Point(588, 163);
+            this.cboHuyDangKy.Name = "cboHuyDangKy";
+            this.cboHuyDangKy.Size = new System.Drawing.Size(181, 24);
+            this.cboHuyDangKy.TabIndex = 12;
+            // 
+            // btnHuyDangKy
+            // 
+            this.btnHuyDangKy.Location = new System.Drawing.Point(473, 163);
+            this.btnHuyDangKy.Name = "btnHuyDangKy";
+            this.btnHuyDangKy.Size = new System.Drawing.Size(97, 23);
+            this.btnHuyDangKy.TabIndex = 13;
+            this.btnHuyDangKy.Text = "Hủy Đăng Ký";
+            this.btnHuyDangKy.UseVisualStyleBackColor = true;
+            this.btnHuyDangKy.Click += new System.EventHandler(this.btnHuyDangKy_Click);
             // 
             // FormDangKyLopHoc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHuyDangKy);
+            this.Controls.Add(this.cboHuyDangKy);
+            this.Controls.Add(this.dataGridViewLopDaDangKy);
+            this.Controls.Add(this.lblLopDangKY);
+            this.Controls.Add(this.btnQuayLai);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.lblCon);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblNgayHomNay);
+            this.Controls.Add(this.lblMalop);
+            this.Controls.Add(this.cboMaLop);
+            this.Controls.Add(this.lblTenSinhVien);
+            this.Controls.Add(this.lblMaSinhVien);
             this.Name = "FormDangKyLopHoc";
             this.Text = "Đăng ký lớp học";
+            this.Load += new System.EventHandler(this.FormDangKyLopHoc_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLopDaDangKy)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMaSinhVien;
+        private System.Windows.Forms.Label lblTenSinhVien;
+        private System.Windows.Forms.ComboBox cboMaLop;
+        private System.Windows.Forms.Label lblMalop;
+        private System.Windows.Forms.Label lblNgayHomNay;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblCon;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnQuayLai;
+        private System.Windows.Forms.Label lblLopDangKY;
+        private System.Windows.Forms.DataGridView dataGridViewLopDaDangKy;
+        private System.Windows.Forms.ComboBox cboHuyDangKy;
+        private System.Windows.Forms.Button btnHuyDangKy;
     }
 }
--- BTLQUANLYSINHVIEN/Form8.cs
+++ BTLQUANLYSINHVIEN/Form8.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,168 @@ namespace BTLQUANLYSINHVIEN
         public FormDangKyLopHoc()
         {
             InitializeComponent();
+        }
+
+        private void FormDangKyLopHoc_Load(object sender, EventArgs e)
+        {
+            lblMaSinhVien.Text = "Mã sinh viên: " + NhoTamThoi.MaSV;
+            lblTenSinhVien.Text = "Tên sinh viên: " + NhoTamThoi.TenSV;
+            lblNgayHomNay.Text = "Ngày hôm nay: " + DateTime.Now.ToString("dd/MM/yyyy");
+
+            LoadLopConMo();
+            LoadLopDaDangKy();
+        }
+        private void LoadLopDaDangKy()
+        {
+            string connStr = "Data Source=.;Initial Catalog=QLSinhVien;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string query = @"SELECT l.MaLop, l.TenLop, l.MaMon, l.Thu, l.Tiet, l.Phong, l.NgayMo, l.NgayDong
+                         FROM tblDangKy dk
+                         JOIN tblLop l ON dk.MaLop = l.MaLop
+                         WHERE dk.MaSV = @MaSV";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                da.SelectCommand.Parameters.AddWithValue("@MaSV", NhoTamThoi.MaSV);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridViewLopDaDangKy.DataSource = dt;
+
+                // Gán cho combobox hủy đăng ký
+                cboHuyDangKy.DataSource = dt;
+                cboHuyDangKy.DisplayMember = "MaLop";
+                cboHuyDangKy.ValueMember = "MaLop";
+            }
+        }
+        private void LoadLopConMo()
+        {
+            string connStr = "Data Source=.;Initial Catalog=QLSinhVien;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string query = @"SELECT l.MaLop, l.TenLop, l.MaMon, l.MaGV, l.Thu, l.Tiet, l.Phong, l.SiSoToiDa,
+                                (l.SiSoToiDa - COUNT(dk.MaSV)) AS ConLai,
+                                l.NgayMo, l.NgayDong
+                         FROM tblLop l
+                         LEFT JOIN tblDangKy dk ON l.MaLop = dk.MaLop
+                         WHERE GETDATE() BETWEEN l.NgayMo AND l.NgayDong
+                         GROUP BY l.MaLop, l.TenLop, l.MaMon, l.MaGV, l.Thu, l.Tiet, l.Phong, l.SiSoToiDa, l.NgayMo, l.NgayDong";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+
+                cboMaLop.DataSource = dt;
+                cboMaLop.DisplayMember = "MaLop";
+                cboMaLop.ValueMember = "MaLop";
+            }
+        }
+
+        private void cboMaLop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboMaLop.SelectedItem != null)
+            {
+                DataRowView row = (DataRowView)cboMaLop.SelectedItem;
+                int conLai = Convert.ToInt32(row["ConLai"]);
+                int siSoToiDa = Convert.ToInt32(row["SiSoToiDa"]);
+                lblCon.Text = $"Còn: {conLai}/{siSoToiDa}";
+            }
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+            if (cboMaLop.SelectedItem == null)
+            {
+                MessageBox.Show("Vui lòng chọn lớp!");
+                return;
+            }
+
+            DataRowView row = (DataRowView)cboMaLop.SelectedItem;
+            string maLop = row["MaLop"].ToString();
+            int conLai = Convert.ToInt32(row["ConLai"]);
+
+            if (conLai <= 0)
+            {
+                MessageBox.Show("Lớp đã đủ sĩ số, không thể đăng ký!");
+                return;
+            }
+
+            string connStr = "Data Source=.;Initial Catalog=QLSinhVien;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "INSERT INTO tblDangKy (MaSV, MaLop, NgayDangKy) VALUES (@MaSV, @MaLop, GETDATE())";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaSV", NhoTamThoi.MaSV);
+                cmd.Parameters.AddWithValue("@MaLop", maLop);
+
+                try
+                {
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows > 0)
+                    {
+                        MessageBox.Show("Đăng ký thành công!");
+                        LoadLopConMo();      // refresh lớp còn mở
+                        LoadLopDaDangKy();   // refresh lớp đã đăng ký
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 2627) // PK violation (đã đăng ký lớp này)
+                    {
+                        MessageBox.Show("Bạn đã đăng ký lớp này rồi!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Có lỗi xảy ra: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnHuyDangKy_Click(object sender, EventArgs e)
+        {
+            if (cboHuyDangKy.SelectedItem == null)
+            {
+                MessageBox.Show("Vui lòng chọn lớp cần hủy!");
+                return;
+            }
+
+            string maLop = cboHuyDangKy.SelectedValue.ToString();
+
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn hủy đăng ký lớp " + maLop + " ?",
+                                                  "Xác nhận hủy",
+                                                  MessageBoxButtons.YesNo,
+                                                  MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                string connStr = "Data Source=.;Initial Catalog=QLSinhVien;Integrated Security=True";
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                    string query = "DELETE FROM tblDangKy WHERE MaSV=@MaSV AND MaLop=@MaLop";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@MaSV", NhoTamThoi.MaSV);
+                    cmd.Parameters.AddWithValue("@MaLop", maLop);
+
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows > 0)
+                    {
+                        MessageBox.Show("Hủy đăng ký thành công!");
+                        LoadLopConMo();      // refresh lớp còn mở
+                        LoadLopDaDangKy();   // refresh lớp đã đăng ký
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy lớp để hủy!");
+                    }
+                }
+
+            }
         }
     }
 }
